4c9828859870a89642648cf096df7abcdb92731c
Habib23me <habib23me@gmail.com>
2019-09-05 09:55:58 +0300

    Vuala

--- demo.dart
+++ demo.dart
@@ -4,5 +4,11 @@ return myProduct??Product();
 
 myProduct??= Product();
 
-List<Product> productList=List.from(this.myProducts)..add(Product());
+//The old way
+List<Product> productList=List<Product>.from(this.myProducts);
+productList.add(Product);
+return productList;
+
+//Easier way
+return List<Product>.from(this.myProducts)..add(Product());
 
